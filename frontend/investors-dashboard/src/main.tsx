0efe1c3543c3c599cd46122690d718d16227766f
AxSch <alex_schuneman@outlook.com>
2024-04-08 20:45:40 +0100

    Refactor to use store & persistence

--- frontend/investors-dashboard/src/main.tsx
+++ frontend/investors-dashboard/src/main.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import { PersistGate } from 'redux-persist/integration/react'
+import { store, persistedStore } from './store/store';
+import { Provider } from 'react-redux'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+      <Provider store={store}>
+          <PersistGate loading={null} persistor={persistedStore}>
+            <App />
+          </PersistGate>
+      </Provider>
   </React.StrictMode>,
 )
