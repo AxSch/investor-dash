b85a4af67b62b20530b8602eeac9a00887098201
AxSch <alex_schuneman@outlook.com>
2024-04-08 17:28:45 +0100

    Add express endpoints

---
+++ frontend/investors-dashboard/api/index.ts
@@ -0,0 +1,84 @@
+import express, * as expressComplete from "express";
+import { Request, Response } from "@types/express";
+import fetch from "node-fetch";
+import {validationResult, query } from "express-validator";
+
+interface Investor {
+    firmId: number,
+    firmName: string,
+    AUM: number,
+    dateAdded: string,
+    lastUpdated: string,
+    establishedAt: string,
+    firmType: string,
+    city: string,
+    country: string,
+    address: string,
+    postal_code: string,
+}
+
+interface InvestorCommitment {
+    id: number,
+    assetClass: string,
+    firmId: number,
+    currency: string,
+    amount: string
+}
+
+export const app = express ? express() : expressComplete();
+if (!process.env['VITE']) {
+    const frontendFiles = process.cwd() + '/dist'
+    app.use(express.static(frontendFiles))
+    app.get('/*', (_, res) => {
+        res.send(frontendFiles + '/index.html')
+    })
+    app.listen(process.env['PORT'])
+}
+
+app.get('/api/investors', async(req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        res.json({redirectUrl: '/'});
+        return;
+    }
+
+    const url = process.env.INVESTOR_API + `api/investors`;
+    try {
+        const response = await fetch(url);
+        const data = await response.json() as Investor[];
+        return res.json({
+            investors: data
+        });
+    } catch (e) {
+        console.error(e.message);
+        res.status(500).json({ error: 'Bad Request' });
+        return;
+    }
+});
+
+const investorQueries = [
+    query('investorId').notEmpty().escape(),
+    query('assetClass').notEmpty().escape()
+];
+
+app.get('/api/commitment/:assetClass/:investorId', investorQueries, async(req: Request, res: Response) => {
+    const errors = validationResult(req);
+    console.info(errors);
+    if (!errors.isEmpty()) {
+        res.status(400).json({ errors: errors.array() });
+        return;
+    }
+    const url = process.env.INVESTOR_API + `api/commitment/investor/${req.query.assetClass}/${req.query.investorId}`;
+
+    try {
+        const response = await fetch(url);
+        const data = await response.json() as InvestorCommitment[];
+        return res.json({
+            investors: data
+        });
+    } catch (e) {
+        console.error(e.message);
+        res.status(500).json({ error: 'Bad Request' });
+        return;
+    }
+});
