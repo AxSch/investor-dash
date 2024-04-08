23b463302d5c9f7d582010079e689b2e2b3d9c8a
AxSch <alex_schuneman@outlook.com>
2024-04-08 21:05:06 +0100

    WIP: Refactor to use store dispatch & data fetching of Investors

--- frontend/investors-dashboard/src/App.tsx
+++ frontend/investors-dashboard/src/App.tsx
@@ -1,33 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect } from 'react'
+import { useAppDispatch, useAppSelector } from "./store/hooks";
+import {selectInvestors, setInvestors} from "./reducers/investorsSlice";
+import { Investors } from "../interfaces/Investors";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const dispatch = useAppDispatch();
+  const investors = useAppSelector(selectInvestors)
+      useEffect(() => {
+        const fetchInvestorServices = async () => {
+            const response = await fetch('/api/investors');
+            const data = await response.json() as Investors;
+            dispatch(setInvestors(data.investors));
+        }
+        if (investors === null) {
+            fetchInvestorServices();
+        }
+    }, [dispatch])
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        {investors ? <div>
+            Render investor table
+        </div> : <div>Loading...</div>}
     </>
   )
 }
