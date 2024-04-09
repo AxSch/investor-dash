e8b68f02f33315d765d4f9208f4368fccafa3118
AxSch <alex_schuneman@outlook.com>
2024-04-09 11:17:34 +0100

    Refactor investorsSlice to handle data fetching

--- frontend/investors-dashboard/src/reducers/investorsSlice.ts
+++ frontend/investors-dashboard/src/reducers/investorsSlice.ts
@@ -1,25 +1,60 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import type { PayloadAction } from "@reduxjs/toolkit";
 import type { RootState } from "../store/store";
 import { Investors } from "../../interfaces/Investors";
 
+interface ApiError {
+    errorMessage: string,
+    status: number,
+}
+
 interface InvestorsState {
-    investors: Investors | null,
-    length: number,
+    data: Investors | null,
+    loading: boolean,
+    error: ApiError | undefined,
 }
 
 const initialState: InvestorsState = {
-    investors: null,
-    length: 0,
+    data: null,
+    loading: false,
+    error: undefined,
 }
+
+export const fetchInvestors = createAsyncThunk<Investors, void, { rejectValue: ApiError }>(
+    'investors/fetchInvestors',
+    async (_, { rejectWithValue }) => {
+        try {
+            const res = await fetch('/api/investors');
+            if (res.status !== 200) {
+                const errorData = await res.json();
+                return rejectWithValue(errorData as ApiError);
+            }
+            const data = await res.json();
+            return data as Investors;
+        } catch (error) {
+            return rejectWithValue(error as ApiError);
+        }
+    }
+);
 
 export const investorSlice = createSlice({
     name: 'investors',
     initialState,
-    reducers: {
-        setInvestors: (state, action: PayloadAction<Investors>) => {
-            state.investors = action.payload;
-        },
+    reducers: {},
+    extraReducers: (builder) => {
+        builder.addCase(fetchInvestors.pending, (state) => {
+            state.loading = true;
+            state.error = null;
+        });
+        builder.addCase(fetchInvestors.fulfilled, (state, action: PayloadAction<Investors>) => {
+            state.loading = false;
+            state.data = action.payload;
+        });
+        builder.addCase(fetchInvestors.rejected, (state , action: PayloadAction<ApiError | null>) => {
+            state.loading = false;
+            state.data = null;
+            state.error = action.payload;
+        });
     },
 });
 
