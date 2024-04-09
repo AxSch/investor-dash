b259ba5e207715efb4ef4933473b929bd4ea5ad5
AxSch <alex_schuneman@outlook.com>
2024-04-09 11:12:37 +0100

    Refactor store useAppDispatch & useAppSelector hooks

--- frontend/investors-dashboard/src/store/hooks.ts
+++ frontend/investors-dashboard/src/store/hooks.ts
@@ -1,5 +1,6 @@
-import { useDispatch, useSelector } from "react-redux"
+import { useDispatch, useSelector, TypedUseSelectorHook } from "react-redux"
 import type { RootState, AppDispatch } from "./store"
+import {ThunkDispatch} from "@reduxjs/toolkit";
 
-export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
-export const useAppSelector = useSelector.withTypes<RootState>();
+export const useAppDispatch = () => useDispatch<ThunkDispatch<AppDispatch, any, any>>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
