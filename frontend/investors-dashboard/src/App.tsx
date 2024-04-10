761653e766312d665fe3e3183ebfb364c5001d3b
AxSch <alex_schuneman@outlook.com>
2024-04-10 17:59:25 +0100

    Refactor to use RouteErrorFallback

--- frontend/investors-dashboard/src/App.tsx
+++ frontend/investors-dashboard/src/App.tsx
@@ -2,11 +2,12 @@ import React from "react";
 import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider} from "react-router-dom";
 import InvestorsDashBoard from "./Pages/InvestorsDashboard/InvestorsDashBoard";
 import Header from "./Components/Header/Header";
+import RouteErrorFallback from "./Components/Error/RouteErrorFallback";
 
 const router = createBrowserRouter(
     createRoutesFromElements(
         <Route path="/" element={<Header />}>
-          <Route index element={<InvestorsDashBoard />} />
+          <Route index element={<InvestorsDashBoard />} errorElement={<RouteErrorFallback />} />
           {/*<Route path="investor" element={<InvestorPage />} />*/}
         </Route>
     )
