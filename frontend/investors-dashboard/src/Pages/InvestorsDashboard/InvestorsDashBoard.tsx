f4462688fe472384ea58a4bef68b9f979a475eaa
AxSch <alex_schuneman@outlook.com>
2024-04-11 01:01:57 +0100

    Refactor Dashboard to be leaner & cleaner

--- frontend/investors-dashboard/src/Pages/InvestorsDashboard/InvestorsDashBoard.tsx
+++ frontend/investors-dashboard/src/Pages/InvestorsDashboard/InvestorsDashBoard.tsx
@@ -1,27 +1,27 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect } from "react"
 import { useAppDispatch, useAppSelector } from "../../store/hooks";
 import { fetchInvestors, selectInvestors } from "../../reducers/investorsSlice";
 import InvestorTable from "../../Components/InvestorTable/InvestorTable";
-import { Investors } from "../../../interfaces/Investors";
 import { useErrorBoundary, withErrorBoundary } from "react-error-boundary";
 import ErrorFallback from "../../Components/Error/ErrorFallback";
+import {Investors} from "../../../interfaces/Investors";
+import {ApiError} from "../../../interfaces/Errors";
 
 const InvestorDashboard: React.FC<{}> = () => {
-    const [loading, setLoading] = useState<boolean>(false);
-    const [investors, setInvestors] = useState<Investors>();
     const { showBoundary } = useErrorBoundary();
     const dispatch = useAppDispatch();
-    const investorsData = useAppSelector(selectInvestors);
+    const { loading, data: investors, error } = useAppSelector<{ loading: boolean, data: Investors, error: ApiError }>(selectInvestors);
 
     useEffect(() => {
-        dispatch(fetchInvestors());
-        setLoading(investorsData.loading);
-        setInvestors(investorsData.data);
-
-        if (investorsData.error) {
-            showBoundary(investorsData.error);
+        if (!investors || !investors.investors) {
+            dispatch(fetchInvestors());
         }
-    }, [dispatch]);
+
+        if (error) {
+            showBoundary(error);
+        }
+    }, [dispatch, showBoundary, investors, error]);
+
     return (
         <div className="container mx-auto">
             {loading && <div>Loading...</div>}
