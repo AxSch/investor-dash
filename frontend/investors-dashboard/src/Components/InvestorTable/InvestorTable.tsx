bae5cebdbf0aa19fef4a382e381af0b57ec1f246
AxSch <alex_schuneman@outlook.com>
2024-04-12 10:25:18 +0100

    Refactor styling of wrapper div

--- frontend/investors-dashboard/src/Components/InvestorTable/InvestorTable.tsx
+++ frontend/investors-dashboard/src/Components/InvestorTable/InvestorTable.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { Investor, Investors } from 'interfaces/Investors';
-import InvestorRow from './InvestorRow';
+import React from "react";
+import { Investor, Investors } from "interfaces/Investors";
+import InvestorRow from "./InvestorRow";
 import { formatHeaders } from "../../utils/formatHeaders";
 import { sortInvestorsHeadings } from "../../utils/sortInvestorsHeaders";
 import InvestorCards from "../InvestorCards/InvestorCards";
@@ -15,7 +15,7 @@ const InvestorTable: React.FC<InvestorTableProps> = ({ data }) => {
 
     return (
         <>
-            <div className="overflow-x-auto mx-auto rounded-lg shadow m-8 max-w-fit">
+            <div className="overflow-x-auto mx-auto rounded-lg shadow m-2 max-w-fit">
                 <table className="divide-y divide-gray-200 table-auto hidden md:block">
                     <thead className="bg-gray-200">
                     <tr>
