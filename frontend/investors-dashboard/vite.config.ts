5edbb268f6ea4e357a7f3b1c5c48369ca325dcca
AxSch <alex_schuneman@outlook.com>
2024-04-12 10:29:19 +0100

    Refactor vite config based on lint errors

--- frontend/investors-dashboard/vite.config.ts
+++ frontend/investors-dashboard/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import express from './express-plugin'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command, mode }) => {
+export default defineConfig(() => {
   return {
     plugins: [react(), express('/api')],
   }
