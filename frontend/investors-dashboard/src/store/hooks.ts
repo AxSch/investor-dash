579583025d000c3d1150f03f248d01ee10a68ae4
AxSch <alex_schuneman@outlook.com>
2024-04-12 10:28:58 +0100

    Refactor hooks based on lint errors

--- frontend/investors-dashboard/src/store/hooks.ts
+++ frontend/investors-dashboard/src/store/hooks.ts
@@ -1,6 +1,6 @@
 import { useDispatch, useSelector, TypedUseSelectorHook } from "react-redux"
 import type { RootState, AppDispatch } from "./store"
-import {ThunkDispatch} from "@reduxjs/toolkit";
+import {UnknownAction, ThunkDispatch} from "@reduxjs/toolkit";
 
-export const useAppDispatch = () => useDispatch<ThunkDispatch<AppDispatch, any, any>>();
+export const useAppDispatch = () => useDispatch<ThunkDispatch<AppDispatch, unknown, UnknownAction>>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
