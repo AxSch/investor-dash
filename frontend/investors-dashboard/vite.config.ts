0641d128aa4f445d37f1688baa52970deb885f54
AxSch <alex_schuneman@outlook.com>
2024-04-13 21:24:04 +0100

    Setup vitest

--- frontend/investors-dashboard/vite.config.ts
+++ frontend/investors-dashboard/vite.config.ts
@@ -1,10 +1,17 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client />
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import express from './express-plugin'
 
 // https://vitejs.dev/config/
-export default defineConfig(() => {
-  return {
+export default defineConfig({
     plugins: [react(), express('/api')],
-  }
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      setupFiles: './src/test/setup.ts',
+      css: true
+    }
 })
