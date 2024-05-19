52b78df1d38ed175439e79f5beb99af64247a08e
AxSch <alex_schuneman@outlook.com>
2024-05-19 18:07:55 +0100

    Remove RTK config & files

--- frontend/investors-dashboard/src/store/hooks.ts
+++
@@ -1,6 +0,0 @@
-import { useDispatch, useSelector, TypedUseSelectorHook } from "react-redux"
-import type { RootState, AppDispatch } from "./store"
-import {UnknownAction, ThunkDispatch} from "@reduxjs/toolkit";
-
-export const useAppDispatch = () => useDispatch<ThunkDispatch<AppDispatch, unknown, UnknownAction>>();
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
