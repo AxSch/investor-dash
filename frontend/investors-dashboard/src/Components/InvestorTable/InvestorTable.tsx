f534c922998aa75cbfd2be099b8d876f46ac9a0a
AxSch <alex_schuneman@outlook.com>
2024-05-19 18:05:15 +0100

    Refactor component to use ag-grid

--- frontend/investors-dashboard/src/Components/InvestorTable/InvestorTable.tsx
+++ frontend/investors-dashboard/src/Components/InvestorTable/InvestorTable.tsx
@@ -1,39 +1,42 @@
-import React from "react";
-import { Investor, Investors } from "interfaces/Investors";
-import InvestorRow from "./InvestorRow";
-import { formatHeaders } from "../../utils/formatHeaders";
-import { sortInvestorsHeadings } from "../../utils/sortInvestorsHeaders";
-import InvestorCards from "../InvestorCards/InvestorCards";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Investor } from "interfaces/Investors";
+import { AgGridReact } from 'ag-grid-react';
+import "ag-grid-community/styles/ag-grid.css";
+import "ag-grid-community/styles/ag-theme-quartz.css";
 
 interface InvestorTableProps {
-    data: Investors;
+    data: Investor[];
 }
 
 const InvestorTable: React.FC<InvestorTableProps> = ({ data }) => {
-    const numOfKeys = Object.keys(data.investors![0]).length;
-    const headings = sortInvestorsHeadings(Object.keys(data.investors![0]).slice(0, numOfKeys -4).map(formatHeaders) as string[]);
+    const navigate = useNavigate();
+    const [colDefs, setColDefs] = useState([]);
+    const [rowData, setRowData] = useState([]);
+
+    useEffect(() => {
+        const setTableData = () => {
+            const columnHeadings = Object.keys(data![0]).map(heading => {
+                return {
+                    field: heading,
+                    filter: true,
+                }
+            });
+            setRowData(data!);
+            setColDefs(columnHeadings);
+        }
+        setTableData();
+    }, []);
+
+    const handleClick = (investor: Investor) => {
+        navigate(`/investor/${investor.firmId}`);
+    };
 
     return (
         <>
-            <div className="overflow-x-auto mx-auto rounded-lg shadow m-2 max-w-fit">
-                <table data-testid="investor-table" className="divide-y divide-gray-200 table-auto hidden md:block">
-                    <thead className="bg-gray-200">
-                    <tr>
-                        {headings.map((heading: string) => (
-                            <th key={heading} scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                {heading}
-                            </th>
-                        ))}
-                    </tr>
-                    </thead>
-                    <tbody className="bg-white divide-y divide-gray-200">
-                    {data.investors!.map((investor: Investor) => (
-                        <InvestorRow key={investor.firmId} investor={investor} />
-                    ))}
-                    </tbody>
-                </table>
+            <div className="ag-theme-quartz-dark w-100" style={{ height: '15rem'}}>
+                <AgGridReact rowData={rowData} columnDefs={colDefs} ensureDomOrder={true} onRowClicked={(e) => handleClick(e.data)} />
             </div>
-            <InvestorCards data={data} />
         </>
     );
 };
