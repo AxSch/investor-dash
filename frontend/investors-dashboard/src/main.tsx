db75fd29a719340e5342897e4e7681889e3f0bd1
AxSch <alex_schuneman@outlook.com>
2024-05-19 17:59:12 +0100

    Replace redux provider with react-query

--- frontend/investors-dashboard/src/main.tsx
+++ frontend/investors-dashboard/src/main.tsx
@@ -1,17 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import App from './App.tsx'
-import { PersistGate } from 'redux-persist/integration/react'
-import { store, persistedStore } from './store/store';
-import { Provider } from 'react-redux'
 import './index.css'
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-      <Provider store={store}>
-          <PersistGate loading={null} persistor={persistedStore}>
+      <QueryClientProvider client={queryClient}>
             <App />
-          </PersistGate>
-      </Provider>
+      </QueryClientProvider>
   </React.StrictMode>,
 )
