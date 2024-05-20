5ba5ecc955554d119c814eb56846354aafee7c9f
AxSch <alex_schuneman@outlook.com>
2024-05-20 12:25:47 +0100

    Add formatting of specific column data

--- frontend/investors-dashboard/src/Components/InvestorTable/InvestorTable.tsx
+++ frontend/investors-dashboard/src/Components/InvestorTable/InvestorTable.tsx
@@ -1,9 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState} from "react";
 import { useNavigate } from "react-router-dom";
 import { Investor } from "interfaces/Investors";
 import { AgGridReact } from 'ag-grid-react';
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-quartz.css";
+import { formatDate } from "../../utils/formatDate";
+import { formatAUM } from "../../utils/formatAUM";
 
 interface InvestorTableProps {
     investors: Investor[];
@@ -17,6 +19,18 @@ const InvestorTable: React.FC<InvestorTableProps> = ({ investors }) => {
     useEffect(() => {
         const setTableData = () => {
             const columnHeadings = Object.keys(investors![0]).map(heading => {
+                if(heading === 'dateAdded' || heading === 'establishedAt' || heading === 'lastUpdated') {
+                    return {
+                        field: heading,
+                        valueFormatter: params => formatDate(params.value),
+                    }
+                }
+                if(heading === 'AUM') {
+                    return {
+                        field: heading,
+                        valueFormatter: params => formatAUM(params.value),
+                    }
+                }
                 return {
                     field: heading,
                 }
