c59c6a8e6fb9dc06e2f9e0267dff6643a64eb4d8
AxSch <alex_schuneman@outlook.com>
2024-05-20 09:27:25 +0100

    Refactor to use isLoading

--- frontend/investors-dashboard/src/Pages/InvestorsDashboard/InvestorsDashBoard.tsx
+++ frontend/investors-dashboard/src/Pages/InvestorsDashboard/InvestorsDashBoard.tsx
@@ -8,7 +8,7 @@ import { fetchInvestors } from "../../api/fetchInvestors";
 const InvestorsDashboard: React.FC = () => {
     const { showBoundary } = useErrorBoundary();
 
-    const { isPending, error, data: investors, isFetching } = useQuery({
+    const { isLoading, error, data: investors } = useQuery({
         queryKey: ['investors'],
         queryFn: fetchInvestors,
         select: (data) => data.investors,
@@ -25,7 +25,7 @@ const InvestorsDashboard: React.FC = () => {
             <div className="my-8 flex justify-center">
                 <h1 className="text-3xl font-bold" data-testid="investors-heading">Investors</h1>
             </div>
-            {isPending || isFetching && <div className="flex justify-center" data-testid="investors-loading">Loading...</div>}
+            {isLoading && <div className="flex justify-center" data-testid="investors-loading">Loading...</div>}
             {investors && <InvestorTable data={investors} />}
         </div>
     )
